d15f714511b7a0a05c1de735aa10db88580e0c2d
João Henrique <joaohenriquenerestavares23@gmail.com>
2026-08-26 21:56:05 +0000

    Adicionar fundo animado com blobs de gradiente

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,16 +3,20 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Chatbot from "@/components/Chatbot";
 import BackToTop from "@/components/BackToTop";
+import AnimatedBackground from "@/components/AnimatedBackground";
 
 // Shell compartilhado: navbar + conteúdo + rodapé + ferramentas flutuantes
 export default function Layout() {
   return (
-    <div className="flex min-h-screen flex-col prism-gradient">
-      <Navbar />
-      <main className="flex-1 pt-16">
-        <Outlet />
-      </main>
-      <Footer />
+    <div className="relative flex min-h-screen flex-col">
+      <AnimatedBackground />
+      <div className="relative z-10 flex flex-1 flex-col">
+        <Navbar />
+        <main className="flex-1 pt-16">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
       <BackToTop />
       <Chatbot />
     </div>
